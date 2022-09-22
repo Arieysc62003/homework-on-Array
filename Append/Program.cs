136206f307ec2121e240d1885e9d7aaef41fab4a
Arieysc62003 <arieysc62003@gmail.com>
2022-09-22 13:10:52 +0300

    The soltion of append

---
+++ Append/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] arr = { 37, 4, 8, 54, 8, 4, 8, 4 };
+        int[] newArr = new int[arr.Length + 1];
+        for (int i = 0; i < newArr.Length; i++)
+        {
+            newArr[i] = Add(arr, 3)[i];
+            Console.Write(newArr[i]);
+            if (i != newArr.Length - 1)
+                Console.Write(", ");
+        }
+    }
+    static int[] Add(int[] getArr, int getAdd)
+    {
+        int[] add = new int[getArr.Length + 1];
+
+        for (int i = 0; i < getArr.Length; i++)
+        {
+            add[i] = getArr[i];
+        }
+        add[getArr.Length] = getAdd;
+
+        return add;
+
+    }
+}
